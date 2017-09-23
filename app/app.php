33b3a13c53fb50c1c26ccae5b9d1d813e919e848
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-09-22 19:37:53 -0700

    add functionality for account to display last month of services, and for student to display this month and last month of services. Started working on functionality to update a service.

--- app/app.php
+++ app/app.php
@@ -191,6 +191,10 @@
         $selected_student = Student::find($id);
         $notes_array = explode("|", $selected_student->getNotes());
         $assigned_teachers = $selected_student->getTeachers();
+        $this_month=intval(date('m',strtotime('this month')));
+        $this_months_year=intval(date('Y',strtotime('this month')));
+        $last_month=intval(date('m',strtotime('last month')));
+        $last_months_year=intval(date('Y',strtotime('last month')));
         return $app['twig']->render('owner_student.html.twig', array(
             'school' => $school,
             'student' => $selected_student,
@@ -199,7 +203,12 @@
             'courses'=>$school->getCourses(), 'enrolled_courses'=>$selected_student->getCourses(),
             'teachers' => $school->getTeachers(),
             'lessons' => $school->getLessons(),
-            'assigned_lessons' => $selected_student->getLessons()));
+            'assigned_lessons' => $selected_student->getLessons(),
+            'this_month' => $this_month,
+            'this_months_year'=>$this_months_year,
+            'last_month'=>$last_month,
+            'last_months_year'=>$last_months_year
+            ));
     });
 
     //JOIN student to course
@@ -243,6 +252,35 @@
             'lessons' => $school->getLessons(),
             'assigned_lessons' => $selected_student->getLessons()));
     });
+
+    //UPDATE student service NOTE UNTESTED UNTESTED UNTESTED
+    // $app->update("/owner_students/student_student_update_service/{id}, function($id) use($app)" {
+    //
+    //     $school=School::find($_SESSION['school_id']);
+    //     $selected_service = Service::find($id);
+    //     $selected_student = $selected_service->getStudents()[0];
+    //     $notes_array = explode("|", $selected_student->getNotes());
+    //     $assigned_teachers = $selected_student->getTeachers();
+    //     $this_month=intval(date('m',strtotime('this month')));
+    //     $this_months_year=intval(date('Y',strtotime('this month')));
+    //     $last_month=intval(date('m',strtotime('last month')));
+    //     $last_months_year=intval(date('Y',strtotime('last month')));
+    //
+    //     return $app['twig']->render('owner_student.html.twig', array(
+    //       'school' => $school,
+    //       'student' => $selected_student,
+    //       'assigned_teachers' => $assigned_teachers,
+    //       'notes_array' => $notes_array,
+    //       'courses'=>$school->getCourses(), 'enrolled_courses'=>$selected_student->getCourses(),
+    //       'teachers' => $school->getTeachers(),
+    //       'lessons' => $school->getLessons(),
+    //       'assigned_lessons' => $selected_student->getLessons(),
+    //       'this_month' => $this_month,
+    //       'this_months_year'=>$this_months_year,
+    //       'last_month'=>$last_month,
+    //       'last_months_year'=>$last_months_year
+    //     ));
+    // });
 
     //DELETE student from school
     $app->delete("/owner_students/student_termination/{id}", function($id) use ($app) {
@@ -334,6 +372,8 @@
         $selected_courses = $selected_account->getCourses();
         $selected_lessons = $selected_account->getLessons();
         $notes_array = explode("|", $selected_account->getNotes());
+        $last_month = intval(date('m',strtotime('last month')));
+        $last_months_year = intval(date('Y',strtotime('last month')));
 
         return $app['twig']->render('owner_client.html.twig', array('school'=>$school,
         'account'=>$selected_account,
@@ -342,7 +382,9 @@
         'selected_courses'=>$selected_courses,
         'notes_array'=>$notes_array,
         'services'=>$selected_account->getServices(),
-        'selected_lessons'=>$selected_lessons));
+        'selected_lessons'=>$selected_lessons,
+        'last_month'=>$last_month,
+        'last_months_year'=>$last_months_year));
 
     });
 
