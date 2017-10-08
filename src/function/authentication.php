c2353a6219001d60c2cd9fa0fe6b581b73f50d85
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-08 11:47:23 -0700

    add authentication.php

---
+++ src/function/authentication.php
@@ -0,0 +1,29 @@
+<?php
+  function loginOwner($owner) {
+
+      // Renerating the ID protects the admin from session fixation.
+      session_regenerate_id();
+      $_SESSION['owner_id'] = $owner->getId();
+      $_SESSION['last_login'] = time();
+      return true;
+  }
+
+  function isOwnerLoggedIn() {
+    return isset($_SESSION['owner_id']);
+  }
+
+  // Call require_login() at the top of any page which needs to
+  // require a valid login before granting acccess to the page.
+  function isLoggedIn() {
+    if (!isOwnerLoggedIn()) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function logout() {
+    unset($_SESSION['owner_id']);
+    unset($_SESSION['last_login']);
+    return true;
+  }
