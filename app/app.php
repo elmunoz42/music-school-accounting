437f3e67dc8bd00190f56d351fb745bcaeab670f
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-27 08:45:35 -0700

    add SESSION 'role' and before check

--- app/app.php
+++ app/app.php
@@ -64,11 +64,37 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
+    // if user is not logged in, redirect
     $is_logged_in = function ($request, $app) {
       if (!isLoggedIn()) {
           return $app->redirect("/owner_login");
       }
     };
+
+    // if user is not owner, redirect
+    $owner_only = function ($request, $app) {
+      if ($_SESSION['role'] !== 'owner') {
+          // add error
+          return $app->redirect("/owner_main");
+      }
+    };
+
+    // if user is neither owner nor teacher, redirect
+    $teacher_only = function ($request, $app) {
+      if ($_SESSION['role'] !== 'teacher' || $_SESSION['role'] != 'owner') {
+          // add error
+          return $app->redirect("/owner_main");
+      }
+    };
+
+    // if user is neither owner nor client, redirect
+    $client_only = function ($request, $app) {
+      if ($_SESSION['role'] !== 'client' || $_SESSION['role'] != 'owner') {
+          // add error
+          return $app->redirect("/owner_main");
+      }
+    };
+
     //LOGIN
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
