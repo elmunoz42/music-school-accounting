af950d224886aa598d8acf750c46bf692166b9d2
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-12 14:43:25 -0800

    turn off debug mode

--- app/app.php
+++ app/app.php
@@ -7,7 +7,7 @@
     Request::enableHttpMethodParameterOverride();
 
     $app = new Silex\Application();
-    $app['debug'] = true;
+    // $app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
       'twig.path' => [
         __DIR__.'/../web/views',
